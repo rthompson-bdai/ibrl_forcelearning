4e59c65cede3e45ab7d6589143f1a5ea9cc43a88
Skye Thompson <rthompson@theaiinstitute.com>
2024-08-28 20:18:19 +0000

    updated scripts for running exp

--- mw_main/rl_factors_no_force.sh
+++ mw_main/rl_factors_no_force.sh
@@ -43,12 +43,12 @@ num_devices =`nvidia-smi  -L | wc -l`
 n=0
 for env in ${envs[@]}; do
     for factor in ${factors[@]}; do
-        CUDA_VISIBLE_DEVICES=$n python train_rl_mw.py --config_path ../release/cfgs/metaworld/ibrl_basic.yaml  \
+        CUDA_VISIBLE_DEVICES=$n python train_rl_mw.py --config_path ../release/cfgs/metaworld/ibrl_basic_no_force.yaml  \
                                                       --bc_policy ${env}_${factor} \
-                                                      --save_dir rl_models/metaworld/${env}_${factor}_force\
+                                                      --save_dir rl_models/metaworld/${env}_${factor}_no_force\
                                                       --use_wb 1 \
                                                       --wb_exp ${factor}_rl_test \
-                                                      --wb_run ${env}_force \
+                                                      --wb_run ${env}_no_force \
                                                       --norm True
         n=$((n + 1))
         if test $n -eq $num_devices; then
