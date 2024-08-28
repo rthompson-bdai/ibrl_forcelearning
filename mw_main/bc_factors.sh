c32abfec50e5e675f87a18c16e55095568250ab0
Skye Thompson <rthompson@theaiinstitute.com>
2024-08-28 20:18:42 +0000

    fixed bash error

--- mw_main/bc_factors.sh
+++ mw_main/bc_factors.sh
@@ -7,8 +7,8 @@ envs=(  #"Assembly" \
     # "StickPull" \
     # "PegInsertSide" \
     # "Soccer" \
-    "button-press" \
-    "pick-place" \
+    #"button-press" \
+    #"pick-place" \
     #"bin-picking" \
     # "button-press-topdown" \
     # "button-press-topdown-wall" \
@@ -40,14 +40,13 @@ factors=(
 )
 
 #make the name of the dataset path
-num_devices = nvidia-smi  -L | wc -l
+num_devices=`nvidia-smi  -L | wc -l`
 n=0
 for env in ${envs[@]}; do
     for factor in ${factors[@]}; do
         CUDA_VISIBLE_DEVICES=$n python train_bc_mw.py --dataset.path ${env}_${factor} --save_dir models/metaworld/${env}_${factor} --use_wb 1 --wb_exp ${env}_bc_1 --wb_run ${factor} & #> log/metaworld/bc/${env}_${factor}.txt &
         n=$((n + 1))
-        if (($n -eq $num_devices)); then
-            echo waiting
+        if test $n -eq $num_devices; then
             wait -n 
             n=0
         fi
